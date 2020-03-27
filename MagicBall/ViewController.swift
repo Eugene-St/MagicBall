c6cb4b62898d157ab058b4bf9bcf3494f13e5423
Eugene St <Eugene.stepchenko@gmail.com>
2020-03-27 14:05:04 +0200

    Initial Commit

---
+++ MagicBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicBall
+//
+//  Created by Eugene St on 27.03.2020.
+//  Copyright © 2020 Eugene St. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
